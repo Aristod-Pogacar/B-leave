fb9d846155d042cdeaef1fefe9cb849423a9a4a3
randrianiaina3 <hermannrandrianiaina3@gmail.com>
2026-08-28 09:15:28 +0300

    fix: correct partial-type import path for api/leave dto

--- src/api/leave/dto/update-leave.dto.ts
+++ src/api/leave/dto/update-leave.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '../../common/partial-type';
+import { PartialType } from '../../../common/partial-type';
 import { CreateLeaveDto } from './create-leave.dto';
 
 export class UpdateLeaveDto extends PartialType(CreateLeaveDto) {}
